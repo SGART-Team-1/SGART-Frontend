7ff63be08f18fce2f59ee5f316399712ceda4f41
Alberto Cazallas Monje <115149185+AlbertoCazallasMonje@users.noreply.github.com>
2024-10-09 10:11:49 +0200

    Actualización de Google Auth
    Actualización del módulo de Google Auth

--- Sgart_front/src/App.js
+++ Sgart_front/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import GoogleAuth from './components/GoogleAuth';
 import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
       <div className="App">
-          <RegisterForm />
+          <GoogleAuth />
       </div>
   );
 }
--- Sgart_front/src/components/GoogleAuth.js
+++ Sgart_front/src/components/GoogleAuth.js
@@ -25,7 +25,6 @@ const GoogleAuth = () => {
             </div>
             </div>
         </div>
-
     );
 };
 
